de696a1be4e1ee40f4c8169b2576ada128a05651
Killua78 <nacimeboubekeur@gmail.com>
2025-06-30 23:02:08 +0200

    js learning

---
+++ script.js
@@ -0,0 +1,34 @@
+// === Exo 1 : variables ===
+
+/**
+
+let prenom ="Nacime";
+
+let age = 25;   
+
+let ville = "Achères";
+
+console.log(`Bonjour je m'appelle ${prenom}, j'ai ${age} ans et j'habite à ${ville}`);
+
+**/
+
+// === Exo 2 : conditions ===
+
+/**
+
+let age = 20;
+
+if(age >= 0 && age < 18){
+    console.log("Tu es mineur");
+}else if(age >= 18){
+    console.log("Tu es majeur");
+}else{
+    console.log("Âge invalide");
+}
+
+**/
+
+// pour gérer le cas où âge n'est pas un nombre : typeof age !== "number" || age < 0
+
+
+
